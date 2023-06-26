08eb19a94508f2aa89709c75d8869b1d462ba498
Cosmin869 <127846890+Cosmin869@users.noreply.github.com>
2023-06-26 13:19:22 +0300

    Update2 ColorPicker.py
    Am adaugat functia de a alege culoarea doar prin mutarea mouse-ului pe un pixel din poza selectata in timp real, iar butonul "Pick a color" a fost reprogramat pentru a alege o culoare dintr-o paleta de culori, unde putem schimba saturatia, si alte aspecte legate de culori

--- ColorPicker.py
+++ ColorPicker.py
@@ -1,16 +1,18 @@
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import colorchooser
 import pyautogui
 import serial
 from PIL import ImageTk, Image
+import time
 
 
 root = tk.Tk()
 root.title("Picker")
-root.geometry("400x400")
+root.geometry("600x600")
 
 imag = Image.open("Color_circle.jpg")
-imag = imag.resize((300, 300))  # Schimbam dimensiunile
+imag = imag.resize((400, 400))  # Schimbam dimensiunile
 photo = ImageTk.PhotoImage(imag)
 image_label = tk.Label(root, image=photo)
 image_label.pack()
@@ -26,14 +28,16 @@ def color(event):
     image_x = root.winfo_rootx() + image_label.winfo_x()
     image_y = root.winfo_rooty() + image_label.winfo_y()
     mx = image_x + event.x
-    my = image_y + event.y
+    my = image_y + event.y 
     color = get_rgb_color(mx, my)
     r,g,b = color
     rgb_color = f'{r},{g},{b}'
     backcolor = '#%02x%02x%02x' % color
     root.configure(background=backcolor)
-    root.unbind("<Button-1>")
+    print(rgb_color)
     send_color_to_arduino(rgb_color)
+    #root.unbind("<Button-1>")
+    
 
 def send_color_to_arduino(rgb_color):
     ser.write(f'{rgb_color}\n'.encode())
@@ -43,17 +47,29 @@ def selectPicture():
     if file_path:
         # Updatam imaginea cu paleta de culori cu noua imagine
         image = Image.open(file_path)
-        image = image.resize((300, 300))  # Modificam dimensiunile
+        image = image.resize((400, 400))  # Modificam dimensiunile
         photo = ImageTk.PhotoImage(image)
         image_label.configure(image=photo)
         image_label.image = photo  # incarcam poza in variabila
 
 def buttonAction():
-    root.bind("<Button-1>", color)
+    choose_color = colorchooser.askcolor()[0]
+    r,g,b = choose_color
+    rgb_color = f'{r},{g},{b}'
+    backcolor = '#%02x%02x%02x' % choose_color
+    root.configure(background=backcolor)
+    print(rgb_color)
+    send_color_to_arduino(rgb_color)
+    time.sleep(1)
+    
+    #print(choose_color)
+
 
 # Butoanele
 PickColorButton = tk.Button(root, text="Pick a color", command=buttonAction)
 PickColorButton.pack()
+root.bind("<Motion>", color)
+#root.bind("",RGB_Spectrum())
 UploadPhotoButton = tk.Button(root, text="Select a Picure", command=selectPicture)
 UploadPhotoButton.pack()
 
